50df278a952eeb3073f99adbbb6828bfb1acd811
“Sandip <sandipkalekar808@gmail.com>
2023-04-30 12:49:55 +0530

    Creating, altering cookies

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -14,12 +14,16 @@ module.exports.signUp=function(req, res){
 
 //render thr sign in page
 module.exports.signIn=function(req,res){
-    return res.render('user_sign_up',{
+    return res.render('user_sign_in',{
         title:"Codeial | Sign In"
     })
 }
 
 //get the sign up data
 module.exports.create=function(req,res){
+    //TODO later
+}
+
+module.exports.createSession=function(req,res){
     //TODO later
 }
--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const express=require('express');
+const cookieParser=require('cookie-parser')
 const app=express();
 const port=8000;
 const expressLayouts=require('express-ejs-layouts');
 const db=require('./config/mongoose');
+
+app.use(express.urlencoded({extended:true}));    //body-parser deprecated undefined extended: provide extended option
+
+app.use(cookieParser());
+app.use(express.static('./assets')); 
 
 app.use(expressLayouts);
 
 //extract styles and scripts from sub pages into the layout
 app.set('layout extractStyles',true);
 app.set('layout extractScripts',true);
-
-app.use(express.static('./assets'));
 
 //use express router
 app.use('/',require('./routes'));   // "/"  is sent to "./routes"
