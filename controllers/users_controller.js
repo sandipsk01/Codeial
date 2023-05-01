836c414a604842b064056b02ab2e2a3b54479bba
“Sandip <sandipkalekar808@gmail.com>
2023-05-01 12:02:53 +0530

    Manual local auth

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,9 +1,21 @@
 const User=require('../models/user');
 
 module.exports.profile=function(req,res){
-    return res.render('user_profile',{
-        title:"Profile Page"
-    }); 
+    if(req.cookies.user_id){
+        User.findById(req.cookies.user_id, function(err,user){
+            if(user){
+                return res.render('user_profile',{
+                    title:"Profile Page",
+                    user:user
+                }); 
+            }else{
+            return res.redirect('/users/sign-in')
+            }
+        });
+    }else{
+        return res.redirect('/users/sign-in');
+    }
+    
 }
 
 //render the sign up page
@@ -28,20 +40,38 @@ module.exports.create=function(req,res){
     }
 
     User.findOne({email:req.body.email},function(err,user){
-        if(err){console.log('error in finding user in signing up'); return};
+        if(err){console.log('error in finding user in signing up'); return}
 
         if(!user){
             User.create(req.body, function(err, user){
-                if(err){console.log('error in creating user while signing up'); return};
+                if(err){console.log('error in creating user while signing up'); return}
 
                 return res.redirect('/users/sign-in');
             })
         }else{
             return res.redirect('back')
         }
-    })
+    });
 }
 
 module.exports.createSession=function(req,res){
-    //TODO later
+    //Steps to authenticate
+    //find the user
+    User.findOne({email:req.body.email},function(err,user){
+        if(err){console.log('error in finding user in signing in'); return}
+        //handle user found
+        if(user){
+            //handle password which doesn't match
+            if(user.password!=req.body.password){
+                return res.redirect('back');
+            }
+            //handle session creation
+            res.cookie('user_id',user.id);
+            return res.redirect('/users/profile')
+        }else{
+            //handle user not found
+            return res.redirect('back');
+        }
+    });
+    
 }
