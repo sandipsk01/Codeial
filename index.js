a28c97723a9082b540a44310ce75b80ca74ed881
“Sandip <sandipkalekar808@gmail.com>
2023-05-04 00:05:53 +0530

    passport.js and mongo store

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const db=require('./config/mongoose');
 const session=require('express-session');
 const passport=require('passport');
 const passportLocal=require('./config/passport-local-strategy');
+const MongoStore=require('connect-mongo');
 
 app.use(express.urlencoded({extended:true}));    //body-parser deprecated undefined extended: provide extended option
 
@@ -23,6 +24,7 @@ app.set('layout extractScripts',true);
 app.set('view engine','ejs');
 app.set('views','./views');
 
+//mongo store is used to store the session cookie in the db
 app.use(session({
     name:'codeial',
     //TODO change the secret before deployment in production mode
@@ -31,13 +33,23 @@ app.use(session({
     resave:false,
     cookie:{   //cookie to give session time
         maxAge:(1000*60*100)
-    }
+    },
+    store: MongoStore.create(
+        {
+            mongoUrl:'mongodb://0.0.0.0/codeial_development',
+            autoRemove:'disabled'
+        },
+        function(err){
+            console.log(err||'connect-mongo setup ok');
+        }
+    )
 }));
 
 
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.use(passport.setAuthenticatedUser);
 //use express router
 app.use('/',require('./routes'));   // "/"  is sent to "./routes"
 
