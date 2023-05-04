2d20f228978eb6f95e0d84ad361a4caa5c3b2b73
“Sandip <sandipkalekar808@gmail.com>
2023-05-04 16:13:02 +0530

    sass and scss

--- index.js
+++ index.js
@@ -9,6 +9,15 @@ const passport=require('passport');
 const passportLocal=require('./config/passport-local-strategy');
 const MongoStore=require('connect-mongo');
 
+const sassMiddleware=require('node-sass-middleware');
+
+app.use(sassMiddleware({
+    src:'./assets/scss',
+    dest:'./assets/css',
+    debug:true,
+    outputStyle:'extended',
+    prefix:'/css'
+}));
 app.use(express.urlencoded({extended:true}));    //body-parser deprecated undefined extended: provide extended option
 
 app.use(cookieParser());
