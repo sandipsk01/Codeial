d9a937020ec2d5efc120f7b8e1d900b50c65b8e9
“Sandip <sandipkalekar808@gmail.com>
2023-05-11 12:08:32 +0530

    Used async await to simplify callback hell

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,7 @@
 const Post = require('../models/post');
 // const { populate } = require('../models/user');
 const User = require('../models/user');
-module.exports.home=function(req, res){
+module.exports.home=async function(req, res){
     // console.log(req.cookies);
     // res.cookie('user_id',25);
     
@@ -11,20 +11,43 @@ module.exports.home=function(req, res){
     //     });  
     // });
 
-    //Populate the user of each post
-    Post.find({})
-    .populate("user")
-    .populate({
-        path: 'comments',   
-        populate: {
-            path: 'user'
-        }
-    })
-    .exec()
-    .then((posts)=>{
-        User.find({})
-        .then((users)=>{
-            return res.render('home',{title:"Codeial | Home", posts:posts, all_users:users})
-        });
-    })
+    //Populate the user of each post using promise
+    // Post.find({})
+    // .populate("user")
+    // .populate({
+    //     path: 'comments',   
+    //     populate: {
+    //         path: 'user'
+    //     }
+    // })
+    // .exec()
+    // .then((posts)=>{
+    //     User.find({})
+    //     .then((users)=>{
+    //         return res.render('home',{title:"Codeial | Home", posts:posts, all_users:users})
+    //     });
+    // })
+
+    //Populate the user of each post using async await
+    try{
+        let posts = await Post.find({})
+        .populate("user")
+        .populate({
+            path: 'comments',   
+            populate: {
+                path: 'user'
+            }
+        })
+    
+        let users = await User.find({});
+
+        return res.render('home',{
+            title:"Codeial | Home",
+            posts:posts,
+            all_users:users}
+        )
+    }catch(err){
+        console.log('Error',err);
+    }
+    
 }
--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,45 +1,86 @@
 const Post= require('../models/post');
 const Comment = require('../models/comment');
-module.exports.create = function(req, res){
-    Post.create({
-        content:req.body.content,
-        user: req.user._id
-    })
-    .catch((err)=>{
-        if(err){console.log('error in creating a post'); return;}
-    })
-    .then((post)=>{
+// By Using promise
+// module.exports.create = function(req, res){
+//     Post.create({
+//         content:req.body.content,
+//         user: req.user._id
+//     })
+//     .catch((err)=>{
+//         if(err){console.log('error in creating a post'); return;}
+//     })
+//     .then((post)=>{
+//         return res.redirect('back');
+//     })
+// }
+
+
+// By using async await
+module.exports.create= async function(req, res){
+    try{
+        await Post.create({
+            content: req.body.content,
+            user: req.user._id
+        });
         return res.redirect('back');
-    })
+
+    }catch(err){
+        console.log('Error', err);
+        return;
+    }
 }
 
-module.exports.destroy = function(req,res){
-    // Post.findById(req.params.id, function(err, post){
-    //     // .id means converting the object id into string
-    //     if(post.user == req.user.id){
-    //         post.remove();
 
-    //         Comment.deleteMany({post: req.params.id}, function(err){
-    //             return res.redirect('back');
-    //         })
-    //     }else{
-    //         return res.redirect('back');
-    //     }
-    // });
-    Post.findById(req.params.id)
-        .then((post)=>{
-        // .id means converting the object id into string
+module.exports.destroy = async function(req,res){
+    try{
+        let post = await Post.findById(req.params.id);
         if(post.user == req.user.id){
             post.deleteOne();
-            Comment.deleteMany({post: req.params.id})
-            .then(()=>{
-                return res.redirect('/');
-            })
-            .catch((err)=>{
-                return res.redirect('/');
-            })
-        }})
-        .catch((err)=>{
-            return res.redirect('/');
-        });
-} 
+            await Comment.deleteMany({post: req.params.id});
+            return res.redirect('back');
+        }else{
+            return res.redirect('back')
+        }
+    }catch(err){
+        console.log('Error',err);
+        return;
+    }
+}
+
+// Old way but its deprecated
+
+// module.exports.destroy = function(req,res){
+//     Post.findById(req.params.id, function(err, post){
+//         // .id means converting the object id into string
+//         if(post.user == req.user.id){
+//             post.remove();
+
+//             Comment.deleteMany({post: req.params.id}, function(err){
+//                 return res.redirect('back');
+//             })
+//         }else{
+//             return res.redirect('back');
+//         }
+//     });
+
+// }
+
+// using promise
+// module.exports.destroy = function(req,res){
+//     Post.findById(req.params.id)
+//         .then((post)=>{
+//         // .id means converting the object id into string
+//         if(post.user == req.user.id){
+//             post.deleteOne();
+//             Comment.deleteMany({post: req.params.id})
+//             .then(()=>{
+//                 return res.redirect('/');
+//             })
+//             .catch((err)=>{
+//                 return res.redirect('/');
+//             })
+//         }})
+//         .catch((err)=>{
+//             return res.redirect('/');
+//         });
+// } 
