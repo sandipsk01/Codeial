fa1912e30eb05555240340144333f9fc5f3c264a
“Sandip <sandipkalekar808@gmail.com>
2023-05-12 19:48:47 +0530

    Converting creating, deleting post to AJAX

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -11,7 +11,11 @@
                 url: '/posts/create',
                 data: newPostForm.serialize(),
                 success: function(data){
-                    console.log(data);
+                    let newPost=newPostDom(data.data.post);
+                    $('#posts-list-container>ul').prepend(newPost);
+
+                    //d.3 populate delete link argument, space is important
+                    deletePost($(' .delete-post-button',newPost))
                 },error:function(error){
                     console.log(error.responseText);
                 }
@@ -21,5 +25,58 @@
 
 
     // Method to create a post in DOM
+    let newPostDom = function(post){
+        return $(`<li id="post-${post._id}">
+                        <p>
+                    
+                            <small>
+                                <a class="delete-post-button" href="/posts/destroy/${post._id}">X</a>
+                            </small>
+                            ${post.content}
+                            <br>
+                            <small>
+                                ${post.user.name}
+                            </small>
+                        </p>
+                        <div class="post-comments">
+                            
+                                <form action="/comments/create" method="POST">
+                                    <input type="text" name="content" placeholder="Type here to add comment...">
+                                    <input type="hidden" name="post" value="${post._id}">
+                                    <input type="submit" value="Add Comment">
+                                </form>
+                            
+                    
+                            <div class="post-comments-list">
+                                <ul id="post-comments-${post._id}">
+                                    
+                                </ul>
+                            </div>
+                        </div>
+                    </li> `)
+    }
+
+
+    //d.method to delete post from DOM
+    // d.1 created a function which sends post id to be deleted
+    let deletePost= function(deleteLink){
+        $(deleteLink).click(function(e){
+            e.preventDefault();
+
+            $.ajax({
+                type:'get',
+                url: $(deleteLink).prop('href'),
+                //recieves some data
+                success: function(data){
+                    $(`#post-${data.data.post_id}`.remove())
+                },error: function(error){
+                    console.log(error.responseText);
+                }
+            });
+        })
+    }
+
+
+
     createPost();
 }
--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -26,7 +26,7 @@ module.exports.create= async function(req, res){
         if(req.xhr){
             return res.status(200).json({
                 data:{
-                    post:post
+                    post: post
                 },
                 message: "Post created"
             })
@@ -49,6 +49,17 @@ module.exports.destroy = async function(req,res){
         if(post.user == req.user.id){
             post.deleteOne();
             await Comment.deleteMany({post: req.params.id});
+
+            //d.2 data contains the post id
+            if(req.xhr){
+                return res.status(200).json({
+                    data:{
+                        post_id:req.params.id
+                    },
+                    message: "Post deleted"
+                })
+            }
+
             req.flash('success','Post and associated comments deleted');
             return res.redirect('back');
         }else{
@@ -56,7 +67,7 @@ module.exports.destroy = async function(req,res){
             return res.redirect('back');
         }
     }catch(err){
-        console.log('Error',err);
+        req.flash('Error',err);
         return res.redirect('back');
     }
 }
